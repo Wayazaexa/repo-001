bdc6967d52bbf59dda73d71c4c8c29ff54da46c8
Wayazaexa <joakimenglund@protonmail.com>
2022-10-21 10:08:18 +0200

    Added the buggy C-file.

---
+++ 1a.c
@@ -0,0 +1,26 @@
+/**
+ * 1a.c
+ * This software copies a string into a buffer, then it prints what it copied to stdout.
+ * This is intentionally buggy and poorly commented code.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char buf[30]; /* a buffer that holds some characters */
+
+	if( argc == 2) {
+		printf("%s\n",argv[1]);
+		strcpy(buf,argv[1]);
+	} else {
+		strcpy(buf,"what can possibly go wrong?");
+	}
+
+	printf("buf = %s\n",buf);
+
+	printf("bye bye ...");
+
+	return 0;
+}
